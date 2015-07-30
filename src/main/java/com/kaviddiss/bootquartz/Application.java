1f00e406fd64309d4b8552653bab8be3f8c81b1a
glide <glidekensington@gmail.com>
2015-07-29 23:00:36 -0700

    Start job on start

--- src/main/java/com/kaviddiss/bootquartz/Application.java
+++ src/main/java/com/kaviddiss/bootquartz/Application.java
@@ -1,15 +1,42 @@
 package com.kaviddiss.bootquartz;
 
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Import;
 
+import com.kaviddiss.bootquartz.job.SampleJob;
+
 @SpringBootApplication
 @Import({SchedulerConfig.class})
-public class Application {
+public class Application implements CommandLineRunner {
+    @Autowired
+    private Scheduler scheduler;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        JobDetail jobDetail = JobBuilder.newJob(SampleJob.class)
+                .storeDurably(true)
+                .build();
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .forJob(jobDetail)
+                .startNow()
+                .build();
+
+        scheduler.scheduleJob(jobDetail, trigger);
+
+        Thread.sleep(5000);
     }
 }
